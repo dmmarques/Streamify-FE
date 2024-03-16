af7102dc004cdbb3b5653cf4aa679860d39ef985
dmmarques <dmiguel.am@gmail.com>
2024-03-16 11:45:46 +0000

    Updated Home Component by adjusting the front caroussel

--- src/layout/HomeComponent.js
+++ src/layout/HomeComponent.js
@@ -8,34 +8,34 @@ const HomeComponent = () => {
                 <Carousel.Item>
                     <img
                         width="100%"
-                        height="300"
+                        height="200"
                         src="https://img.freepik.com/free-photo/yellow-watercolor-paper_95678-446.jpg"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Welcome to Streamify !</h3>
+                        <p>Here you can watch all your favorite movies</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         width="100%"
-                        height="300"
+                        height="200"
                         src="https://static.vecteezy.com/system/resources/previews/005/182/612/non_2x/green-abstract-geometric-shape-background-free-vector.jpg"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Start looking for your favorite movies !</h3>
+                        <p>Completly free !</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         width="100%"
-                        height="300"
+                        height="200"
                         src="https://img.freepik.com/free-photo/abstract-luxury-soft-red-background-christmas-valentines-layout-design-studio-room-web-template-business-report-with-smooth-circle-gradient-color_1258-54521.jpg"
                     />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>Want to know more about us ?</h3>
+                        <p>We have a full page dedicated to that.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
