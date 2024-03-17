76ccca56725c6c7240e228f7957a171898c0e9ab
dmmarques <dmiguel.am@gmail.com>
2024-03-17 00:01:41 +0000

    Updated component to match the pattern

--- src/components/ErrorComponent.js
+++ src/components/ErrorComponent.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const ErrorComponent = () => {
     return (
-        <div className="container-fluid d-flex min-vh-100 justify-content-center align-items-center">
+        <div className="container-fluid d-flex min-vh-100 justify-content-center">
             <table style={{ textAlign: 'center' }}>
                 <tr>
                     <i class="bi bi-exclamation-triangle-fill errorIcon-300 text-warning"></i>
-                </tr>
-                <tr>
-                    <h2>Something went wrong, try again later.</h2>
+                    <h3>Something went wrong... Try again later.</h3>
                 </tr>
             </table>
         </div>
